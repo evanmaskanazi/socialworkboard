043a6f35963fb18c186f2f4f4364a94cae0a6eca
evanmaskanazi <141803891+evanmaskanazi@users.noreply.github.com>
2025-07-09 03:52:56 -0700

    Update new_backend.py

--- new_backend.py
+++ new_backend.py
@@ -14,7 +14,6 @@ import os
 import secrets
 import jwt
 from datetime import datetime, timedelta, date
-import pandas as pd
 from sqlalchemy import and_, or_, func
 import openpyxl
 from openpyxl.styles import Font, PatternFill, Alignment, Border, Side
